a7774328f1b1b469dbee6fe680e5320c68eae8c7
Tobias Östergren <ostergrentobias@gmail.com>
2018-12-17 16:23:31 +0100

    Added a booking class

---
+++ src/tasks/booking/Booking.java
@@ -0,0 +1,33 @@
+package src.tasks.booking;
+
+public class Booking {
+    private int bookingId;
+    private int totalPrice;
+    private String checkInDate;
+    private String checkOutDate;
+
+    public Booking()
+    {
+
+    }
+    public void searchBooking()
+    {
+
+    }
+    public void checkInCustomer() {
+
+    }
+    public void checkOutCustomer()
+    {
+
+    }
+    public void makeABooking()
+    {
+
+    }
+    public void editBooking()
+    {
+
+    }
+
+}
