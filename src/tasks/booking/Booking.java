ffa7b900bea0d029ea683b4602fbe09823bde9a0
Furkan Sadir <furkansadr88@gmail.com>
2018-12-18 15:28:45 +0100

    Added makeBooking class and working on it.

--- src/tasks/booking/Booking.java
+++ src/tasks/booking/Booking.java
@@ -1,5 +1,10 @@
 package src.tasks.booking;
 
+import tasks.customer.Customer;
+
+import javax.naming.Name;
+import java.util.Scanner;
+
 public class Booking {
     private int bookingId;
     private int totalPrice;
@@ -14,15 +19,25 @@ public class Booking {
     {
 
     }
-    public void checkInCustomer() {
-
-    }
     public void checkOutCustomer()
     {
 
     }
-    public void makeABooking()
-    {
+    public void makeABooking() {
+        Scanner input = new Scanner(System.in);
+        Customer myCustomer = new Customer("920404-1312","Alex","Solverborgsgatan 24A","0700224332");
+        System.out.println("Please input your ssn: ");
+        String ssn = input.nextLine();
+
+        while (!ssn.equals(myCustomer.getSsn())){
+            System.out.println("Please input your ssn");
+            ssn = input.nextLine();
+            System.out.println("Available rooms are: " + );
+
+        }
+
+
+
 
     }
     public void editBooking()
